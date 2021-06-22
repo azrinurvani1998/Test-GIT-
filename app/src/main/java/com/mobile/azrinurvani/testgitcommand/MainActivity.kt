3be87a945bfa6194bf05f3409233c638a74a0e64
azrinurvani1998 <azrinurvani1998@gmail.com>
2021-06-22 14:43:20 +0700

    first commit

---
+++ app/src/main/java/com/mobile/azrinurvani/testgitcommand/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.mobile.azrinurvani.testgitcommand
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        //Hallo test first commit dulu ya
+    }
+}
